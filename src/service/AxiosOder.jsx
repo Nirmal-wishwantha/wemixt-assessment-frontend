7211524ce9c6a538446aa8805405da74d36ada16
Nirmal-wishwantha <nirmalwishwantha@gmail.com>
2025-03-03 22:19:16 +0530

    create axios instance

---
+++ src/service/AxiosOder.jsx
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+// const token = localStorage.getItem('login');
+
+const instance = axios.create({
+    baseURL: 'http://localhost:3000',
+    timeout: 1000,
+    headers: {Authorization: `Bearer ${token}`}
+  });
+  export default instance;
