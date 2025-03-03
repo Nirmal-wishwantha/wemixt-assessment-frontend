357b5099f985c1e77b27ba845789f757cb58d33e
Nirmal-wishwantha <nirmalwishwantha@gmail.com>
2025-03-04 00:49:39 +0530

    login and register api call

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,27 +1,57 @@
 import { Box, Button, TextField, Typography, Paper } from '@mui/material';
-import React from 'react';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import instance from '../services/AxiosOder';
 
 export default function LoginPage() {
 
+    const navigate = useNavigate();
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const login = () => {
+        const data = {
+            email: email,
+            password: password
+        };
+    
+        instance
+            .post('/login', data)
+            .then((res) => {
+                console.log(res);
+                localStorage.setItem('wemixt', res.data.data.token);
+    
+                
+                setTimeout(() => {
+                    navigate('/main'); 
+                }, 2000);
+            })
+            .catch((err) => {
+                console.log(err);
+                
+            });
+    };
+    
 
     return (
-        <Box 
-            sx={{ 
-                height: '100vh', 
-                display: 'flex', 
-                alignItems: 'center', 
-                justifyContent: 'center', 
-                 
+        <Box
+            sx={{
+                height: '100vh',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+
             }}
         >
-            <Paper 
-                elevation={6} 
-                sx={{ 
-                    padding: 4, 
-                    width: 350, 
-                    borderRadius: 3, 
-                    textAlign: 'center', 
-                    backgroundColor: 'rgba(255, 255, 255, 0.9)' 
+            <Paper
+                elevation={6}
+                sx={{
+                    padding: 4,
+                    width: 350,
+                    borderRadius: 3,
+                    textAlign: 'center',
+                    backgroundColor: 'rgba(255, 255, 255, 0.9)'
                 }}
             >
                 <Typography variant="h5" fontWeight="bold" mb={2} color="primary">
@@ -29,22 +59,25 @@ export default function LoginPage() {
                 </Typography>
 
                 <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
-                    <TextField label="Email" variant="outlined" fullWidth />
-                    <TextField label="Password" variant="outlined" type="password" fullWidth />
 
-                    <TextField label="Conform Password" variant="outlined" type="password" fullWidth />
-                    <Button 
-                        variant="contained" 
-                        color="primary" 
+                    <TextField label="Email" variant="outlined" fullWidth onChange={(e) => setEmail(e.target.value)} />
+                    <TextField label="Password" variant="outlined" type="password" fullWidth onChange={(e) => setPassword(e.target.value)} />
+
+                    <Button
+                        variant="contained"
+                        color="primary"
                         sx={{ borderRadius: 2, padding: 1.2, fontSize: 16 }}
+                        onClick={login}
                     >
                         Login
                     </Button>
                 </Box>
 
-                <Typography variant="body2" sx={{ marginTop: 2, color: 'gray' }}>
-                    Don't have an account? <span style={{ color: '#6a11cb', cursor: 'pointer' }}>Register</span>
-                </Typography>
+                <Link to={'/register'}>
+                    <Typography component="span" color="primary" sx={{ cursor: "pointer" }}>
+                        Go to Register page
+                    </Typography>
+                </Link>
             </Paper>
         </Box>
     );
